b783f2ecfb5f702f0e977fdef6590b038a1cf4bf
Gabriel Otero <gabriel.otero@ifood.com.br>
2020-08-31 12:08:40 -0300

    Flag to Disabled App Stored in Shared Prefs

--- app/src/main/java/com/otero/qrcodereader/CustomApp.kt
+++ app/src/main/java/com/otero/qrcodereader/CustomApp.kt
@@ -1,6 +1,7 @@
 package com.otero.qrcodereader
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
 import okhttp3.OkHttpClient
 import retrofit2.Call
@@ -10,9 +11,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.system.exitProcess
 
+const val SHARED_KEY = "SharedPrefs"
+const val DISABLED_KEY = "SharedPrefs"
 class CustomApp : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        val sharedPref = getSharedPreferences(SHARED_KEY, Context.MODE_PRIVATE)
 
         val retrofit = Retrofit.Builder()
             .baseUrl("https://yardman-qa.herokuapp.com/")
@@ -27,9 +32,19 @@ class CustomApp : Application() {
         call.enqueue(object : Callback<DisabledDto> {
             override fun onFailure(call: Call<DisabledDto>, t: Throwable) {
                 Log.d("CustomApp", "FAIL")
+                val disabled = sharedPref.getBoolean(DISABLED_KEY, false)
+                if (disabled) {
+                    Log.d("CustomApp", "Disabled")
+                    exitProcess(0)
+                }
             }
 
             override fun onResponse(call: Call<DisabledDto>, response: Response<DisabledDto>) {
+                with (sharedPref.edit()) {
+                    putBoolean(DISABLED_KEY, response.body()!!.disable)
+                    commit()
+                }
+
                 if (response.body()!!.disable) {
                     Log.d("CustomApp", "Disabled")
                     exitProcess(0)
